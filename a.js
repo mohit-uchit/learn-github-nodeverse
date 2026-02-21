dea6bb11c61c5899a897e556f4753e45bbc11dab
detron <detrondzd@gmail.com>
2026-02-21 22:14:44 +0530

    hio

--- a.js
+++ a.js
@@ -1,11 +1,3 @@
 console.log('Hello world')
-<<<<<<< Updated upstream
-console.log('Shraddha')
-console.log('Again')
-<<<<<<< HEAD
-=======
 console.log('Detron')
->>>>>>> Stashed changes
-=======
-console.log('skr')
->>>>>>> 0c1de2bb499ccc3b20fb75435eca566d625f7d0e
+console.log('Detron21')
